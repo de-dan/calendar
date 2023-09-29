0e196a1ca25de4cca3e2f2a969329770241f6e5a
de-dan <d.kasper@gmx.net>
2023-09-29 09:24:42 +0200

    Initial commit

---
+++ android/src/main/java/digital/meinegemeinde/plugins/calendar/Calendar.java
@@ -0,0 +1,11 @@
+package digital.meinegemeinde.plugins.calendar;
+
+import android.util.Log;
+
+public class Calendar {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
